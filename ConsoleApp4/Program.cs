0a178ebaa028c5239a3afba0892e4f1cbc8bb21b
Serafima007 <123898263+Serafima007@users.noreply.github.com>
2023-02-25 20:38:53 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задача 1
+            //Console.Write("n=");
+            //int n = int.Parse(Console.ReadLine());
+            //int[] array = new int[n];
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    Console.WriteLine($"array[{i}]=");
+            //    array[i] = int.Parse(Console.ReadLine());
+            //}
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    Console.WriteLine($"array[{i}]={array[i]}");
+            //}
+
+            //Задача 2
+            //Console.Write("n=");
+            //int n = int.Parse(Console.ReadLine());
+            //int[] array = new int[n];Random rnd = new Random();
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    array[i] = rnd.Next(-20, 20);
+            //}
+            //foreach (int elem in array)
+            //{
+            //    Console.Write($"{elem}\t");
+            //}
+
+            //Задача 3
+            //int[] array = new int[6] {1,7,8,9,2,3};
+            //int max = array[0];
+            //for (int i = 1; i < array.Length; i++)
+            //{
+            //    if (array[i] > max)
+            //    {
+            //        max = array[i]; 
+            //    }
+            //}
+            //int min = array[0];
+            //for (int i = 1; i < array.Length; i++)
+            //{
+            //    if (array[i] < min)
+            //    {
+            //        min = array[i];      
+            //    }
+            //}
+            //Console.WriteLine($"max={max}");
+            //Console.WriteLine($"min={min}");
+
+            Console.ReadKey();
+        }
+    }
+}
